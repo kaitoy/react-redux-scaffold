d1d9e3a611441d374c49bbca63e8f7e619313d67
kaitoy <kaitoy@pcap4j.org>
2018-10-04 08:09:00 +0900

    Introduce redux-logger.

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { createBrowserHistory } from 'history';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { logger } from 'redux-logger';
 import rootSaga from './sagas/rootSaga';
 import rootReducer from './reducers/rootReducer';
 
@@ -11,10 +12,17 @@ const sagaMiddleware = createSagaMiddleware();
 export const history = createBrowserHistory();
 
 export default function configureStore(initialState: Object = {}) {
+  const middlewares = [];
+  if (process.env.NODE_ENV === `development`) {
+    middlewares.push(logger);
+  }
+  middlewares.push(routerMiddleware(history));
+  middlewares.push(sagaMiddleware);
+
   const store = createStore(
     connectRouter(history)(rootReducer),
     initialState,
-    applyMiddleware(routerMiddleware(history), sagaMiddleware),
+    applyMiddleware(...middlewares),
   );
   sagaMiddleware.run(rootSaga);
   return store;
