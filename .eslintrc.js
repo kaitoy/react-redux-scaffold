d6c98d8232683da3fdc2d78a6bf18dfa80448b47
kaitoy <kaitoy@pcap4j.org>
2018-11-25 13:54:29 +0900

    Improve ESLint config such that no-unused-vars:off affects only TypeScript files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,18 @@ module.exports = {
   plugins: ['jest'],
   rules: {
     'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
-    // Set 'no-unused-vars' to off to suppress errors on importing types.
-    // (e.g. error  'FunctionComponent' is defined but never used  no-unused-vars)
-    // Unused vars are checked by TypeScript compiler (at-loader) instead.
-    'no-unused-vars': 'off',
   },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        // Set 'no-unused-vars' to off to suppress errors on importing types.
+        // (e.g. error  'FunctionComponent' is defined but never used  no-unused-vars)
+        // Unused vars are checked by TypeScript compiler (at-loader) instead.
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
   settings: {
     'import/resolver': {
       node: {
